f695768cb4f7d1a3667da824feacc14ba4e616a6
augustsemrau <augu0093@gmail.com>
2022-03-09 15:18:23 +0100

    changed metric computation

--- src/modelling/strategies/metrics/ForecastAccuracy.py
+++ src/modelling/strategies/metrics/ForecastAccuracy.py
@@ -1,41 +1,51 @@
 import numpy as np
 
-def predict_per_flight(passengers_forecasted, passengers_true):
+def forecast_accuracy(loadfactor_forecasted, loadfactor_true, seatcapacity_original):
     '''
     The forecast accuracy is calculated based on the relative difference between the forecasted number of 
     passengers and the actual number of passengers per flight , we will call this the deviation per flight.
  
-    :param passengers_forecasted:       A vector of size N where N is the number of flights. 
-                                        each entry is the forecasted number of flight passengers.
-                                        Such that passengers_forecasted[i] = forecasted number of 
-                                        flight passengers for flight i.
-    :param passengers_true:             A vector of size N where N is the number of fligts. 
-                                        each entry is the true number of flight passengers.
-                                        Such that passengers_true[i] = true number of flight passengers 
+    :param loadfactor_forecasted:       A vector of size N where N is the number of flights. 
+                                        Each entry is the forecasted flight loadfactor.
+                                        Such that loadfactor_forecasted[i] = forecasted loadfactor
+                                        for flight i.
+    :param loadfactor_true:             A vector of size N where N is the number of flights. 
+                                        Each entry is the true flight loadfactor.
+                                        Such that loadfactor_true[i] = true loadfactor
+                                        for flight i.
+    :param seatcapacity_original:       A vector of size N where N is the number of flights. 
+                                        Each entry is the seat capacity of a given flight.
+                                        Such that seatcapacity_original[i] = seat capacity
                                         for flight i.
 
     :returns:                           A vector of size N with the accuracy per flight: 100% - | Deviation per flight |
     '''
-
+    passengers_true = loadfactor_true * seatcapacity_original
+    passengers_forecasted = loadfactor_forecasted * seatcapacity_original
     deviation_per_flight = (passengers_true-passengers_forecasted) / passengers_true
     return 100 - np.abs(deviation_per_flight)
 
-def predict_total(passengers_forecasted, passengers_true):
+def mean_forecast_accuracy(loadfactor_forecasted, loadfactor_true, seatcapacity_original):
     '''
     The forecast accuracy is calculated based on the relative difference between the forecasted number of 
     passengers and the actual number of passengers per flight , we will call this the deviation per flight.
  
-    :param passengers_forecasted:       A vector of size N where N is the number of flights. 
-                                        each entry is the forecasted number of flight passengers.
-                                        Such that passengers_forecasted[i] = forecasted number of 
-                                        flight passengers for flight i.
-    :param passengers_true:             A vector of size N where N is the number of fligts. 
-                                        each entry is the true number of flight passengers.
-                                        Such that passengers_true[i] = true number of flight passengers 
+    :param loadfactor_forecasted:       A vector of size N where N is the number of flights. 
+                                        Each entry is the forecasted flight loadfactor.
+                                        Such that loadfactor_forecasted[i] = forecasted loadfactor
+                                        for flight i.
+    :param loadfactor_true:             A vector of size N where N is the number of flights. 
+                                        Each entry is the true flight loadfactor.
+                                        Such that loadfactor_true[i] = true loadfactor
+                                        for flight i.
+    :param seatcapacity_original:       A vector of size N where N is the number of flights. 
+                                        Each entry is the seat capacity of a given flight.
+                                        Such that seatcapacity_original[i] = seat capacity
                                         for flight i.
 
-    :returns:                           The total accuracy: mean(100% - | Deviation per flight |)
+    :returns:                           The total (mean) forecast accuracy: mean(100% - | Deviation per flight |)
     '''
-
+    passengers_true = loadfactor_true * seatcapacity_original
+    passengers_forecasted = loadfactor_forecasted * seatcapacity_original
     deviation_per_flight = (passengers_true-passengers_forecasted) / passengers_true
     return np.mean(100 - np.abs(deviation_per_flight))
